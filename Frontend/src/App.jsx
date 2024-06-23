61b7a95164ed0958f697dabb14b62ec7ccb5eb7b
DEBASHIS GARAI <debashis1805@gmail.com>
2024-06-23 22:03:55 +0530

    added functionality to add and remove todos

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,27 +4,31 @@ import { LuListTodo } from "react-icons/lu";
 import { MdOutlineDeleteOutline } from "react-icons/md";
 
 function App() {
-  const [showTasks, setShowTasks] = useState([
-    {
-      id: 1,
-      task: "do DSa",
-      desc: "complete the graphy theory basics 8-9",
-      status: "ongoing",
-    },
-    {
-      id: 2,
-      task: "do Webdev",
-      desc: "complete the basics 9-11",
-      status: "ongoing",
-    },
-    {
-      id: 3,
-      task: "do gym",
-      desc: "smash gym from 11-1",
-      status: "ongoing",
-    },
-  ]);
-  const [task, setTask] = useState("");
+  const [showTasks, setShowTasks] = useState([]);
+  const [todotask, setTodoTask] = useState("");
+  const [taskid, setTaskId] = useState(1);
+
+  // function to add TODO
+  const addTodo = () => {
+    setTaskId(taskid + 1);
+    const task = {
+      id: taskid,
+      task: todotask,
+      completed: false,
+    };
+    
+    setShowTasks([...showTasks, task]);
+    setTodoTask("");
+  };
+  
+  // function to delete TODO
+  const deleteTodo = (idx) => {
+    console.log("insidee this function", idx);
+    const temp = [...showTasks];
+    temp.splice(idx, 1);
+    setShowTasks(temp);
+  };
+
   return (
     <>
       <div className="w-full h-screen bg-[#212121] pt-[7rem]">
@@ -39,24 +43,33 @@ function App() {
                 type="text"
                 className="w-full h-[4.1rem] rounded-[3rem] px-[1.5rem] text-2xl focus:outline-none focus:border-none shadow-lg"
                 placeholder="Add your task"
-                value={task}
-                onChange={(e) => setTask(e.target.value)}
+                value={todotask}
+                onChange={(e) => setTodoTask(e.target.value)}
               />
-              <button className="absolute right-[0.75rem] h-[4.1rem] w-[7.5rem] bg-[#212121] rounded-[3rem] font-semibold text-2xl text-white/50 shadow-lg">
+              <button
+                className="absolute right-[0.75rem] h-[4.1rem] w-[7.5rem] bg-[#212121] rounded-[3rem] font-semibold text-2xl text-white/50 shadow-lg"
+                onClick={addTodo}
+              >
                 Add
               </button>
             </div>
             <div className="w-full flex flex-col gap-[0.75rem]">
               {showTasks &&
-                showTasks.map((e) => {
+                showTasks.map((e, idx) => {
                   return (
-                    <div key={e.id} className="text-[#212121] flex text-2xl items-center justify-between bg-white/20 px-[1rem]">
+                    <div
+                      key={e.id}
+                      className="text-[#212121] flex text-2xl items-center justify-between bg-white/50 px-[1rem] py-[0.5rem] rounded-lg"
+                    >
                       <input type="checkbox" />
                       <div className="w-[60%]">
-                      <div>Task : {e.task}</div>
-                      <div className="overflow-hidden whitespace-nowrap text-ellipsis">Description : {e.desc}</div>
+                        <div>{e.task}</div>
+                        {/* <div className="overflow-hidden whitespace-nowrap text-ellipsis">Description : {e.desc}</div> */}
                       </div>
-                      <MdOutlineDeleteOutline className="text-4xl"/>
+                      <MdOutlineDeleteOutline
+                        className="text-4xl cursor-pointer"
+                        onClick={() => deleteTodo(idx)}
+                      />
                     </div>
                   );
                 })}
